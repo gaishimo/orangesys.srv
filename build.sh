8767205d84c02f5aef178ec465bf7e7dc0d495fc
Gavin Zhou <gavin.zhou@gmail.com>
2016-11-12 17:33:23 +0900

    fix test case & add docker tag

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 _tag=$1
 
 if [ -z "${_tag}" ]; then
-    source _VERSION   
+    source _VERSION
     _tag=${_VERSION}
 fi
 
 docker build --tag "orangesys/orangesys-srv:${_tag}" .
+docker tag orangesys/orangesys-srv:${_tag} orangesys/orangesys-srv:latest
